f7ef81032a98928907b1c842e4f6c7eeb428a540
wnlhdx <35994855+wnlhdx@users.noreply.github.com>
2023-04-27 21:02:17 +0800

    python代码

---
+++ python/studytest/main.py
@@ -0,0 +1,33 @@
+import math
+import re
+
+print(2 ** 3)
+print(len(str(2 ** 3)))
+print(math.pi)
+print(math.sqrt(80))
+a = "wlgdc"
+print(a[-1])
+print(a[len(a) - 1])
+print(a[1:3])
+print(a[:-1])
+print(a * 2)
+b = list(a)
+b[1] = 't'
+a.find("dc")
+a.replace("w", "t")
+print(a.split("g"))
+print(a.upper())
+print(a.isalpha())
+a = a + " "
+print(a.rstrip())
+print("%s emm %s" % ("wc", "wc"))
+print('{:2f}'.format(296.2558))
+print('%.2f|%+05d' % (3.14159, -42))
+print(ord("\n"))
+print("\0")
+a = """
+can you
+"""
+print("\u00c4")
+print(a.encode("utf8"))
+print(re.match('can (,*)', a))
