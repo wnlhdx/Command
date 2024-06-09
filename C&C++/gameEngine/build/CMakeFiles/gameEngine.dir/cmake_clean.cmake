c70f1b741aaa3df1901aaeca9c7e37c399260fca
wnlhdx <314460007@qq.com>
2024-06-09 22:51:57 +0800

    vulkan android

---
+++ C&C++/gameEngine/build/CMakeFiles/gameEngine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gameEngine.dir/gameEngine.cpp.o"
+  "CMakeFiles/gameEngine.dir/gameEngine.cpp.o.d"
+  "CMakeFiles/gameEngine.dir/graphicsAPI.cpp.o"
+  "CMakeFiles/gameEngine.dir/graphicsAPI.cpp.o.d"
+  "gameEngine"
+  "gameEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gameEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
