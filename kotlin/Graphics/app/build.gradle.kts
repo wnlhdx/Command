32ccd133febea112245f7ba4f23031ed5168744a
wnlhdx <314460007@qq.com>
2024-06-21 02:25:54 +0800

    first success androidndk

--- kotlin/Graphics/app/build.gradle.kts
+++ kotlin/Graphics/app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
-            version = "3.22.1"
+            version = "3.29.6"
         }
     }
     buildFeatures {
