f6983cdb1e750f48a430a9a250a1edf4eb87c5ee
wnlhdx <35994855+wnlhdx@users.noreply.github.com>
2024-06-23 03:11:39 +0800

    pygame

--- python/rgzn/aitest.py
+++ python/rgzn/aitest.py
@@ -1,10 +1,14 @@
-from openai import OpenAI
+import asyncio
 import os
-import requests
+import edge_tts
+import subprocess
+import time
+from openai import OpenAI
 
+# 假设这是您的正确OpenAI API密钥和基础URL
 client = OpenAI(
-    api_key="sk-VjSCWObgSc3EbfaJQRACXMvb33Q7th40lxF9d7Sk9aJoydQ8",
-    base_url="https://api.chatanywhere.tech/v1"
+    api_key="YOUR_API_KEY",  # 替换为您的OpenAI API密钥
+    base_url="https://api.openai.com/v1"  # 替换为OpenAI官方基础URL
 )
 
 
@@ -13,14 +17,32 @@ def gpt_35_api_stream(input_message):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system",
-             "content": "You are a personal assistant, skilled in all things about a person who wants to know.And I will use you to help me compelet all my work"},
+             "content": "You are a personal assistant, skilled in all things about a person who wants to know.And I "
+                        "will use you to help me compelet all my work"},
             {"role": "user", "content": input_message}
         ]
     )
-    return completion.choices[0].message.content
+    messages = completion.choices[0].message.content
+    print(messages)
+    return messages
+
+
+def save():
+    loop = asyncio.get_event_loop_policy().get_event_loop()
+    try:
+        loop.run_until_complete(amain())
+    finally:
+        loop.close()
+
+
+async def amain() -> None:
+    """Xiaoxiao或者Xiaoyi"""
+    communicate = edge_tts.Communicate(read_message, 'zh-CN-XiaoxiaoNeural')
+    await communicate.save('test.mp3')
 
 
 def read():
+    save()
     if os.name == 'posix':
         # Unix-like 系统，例如 Termux
         read_termux()
@@ -30,16 +52,29 @@ def read():
 
 
 def read_termux():
-    return 0
+    # 播放音频文件
+    subprocess.call('play-audio test.wav', shell=True)
 
 
 def read_windows():
-    return 0
+    # 播放音频文件
+    from pygame import mixer
+
+    mixer.init()
+    size = 0.7
+    mixer.music.set_volume(size)
+    mixer.music.load("./test.mp3")
+    mixer.music.play()
+    time.sleep(5)
 
 
 if __name__ == '__main__':
-    print("我能做些什么？")
-    while 1 == 1:
-        message = input("")
-        print(gpt_35_api_stream(message))
-
+    read_message = "我能做些什么？"
+    print(read_message)
+    read_windows()
+    # while True:
+    #     message = input("")
+    #     if message.lower() == "退出":
+    #         break
+    #     read_message = gpt_35_api_stream(message)
+    #     read()
